948be4af08be01b2cde0b471b66e243618eb5d4b
manjeet <yadumanjeet1234@gmail.com>
2025-07-10 23:12:53 +0530

    Blog context area

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -3,12 +3,17 @@ import logo from "../imgs/logo.png"
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png"
 import { uploadImage } from "../common/aws";
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import { Toaster, toast } from 'react-hot-toast'
+import { EditorContext } from "../pages/editor.pages";
 
 
 const BlogEditor = () => {
     let blogBannerRef = useRef();
+
+    const {blog,blog: {banner, title, des,content,tags},setBlog } = useContext(EditorContext)
+
+    // console.log(blog)
 
     const handleBannerUpload = async (e) => {
 
@@ -23,7 +28,9 @@ const BlogEditor = () => {
                     toast.dismiss(loadingToast);
                     toast.success("Uploaded successfully...")
 
-                    blogBannerRef.current.src = url;
+                    // blogBannerRef.current.src = url;
+
+                    setBlog({...blog, banner:url})
                 }
             }).catch((error) => {
                 toast.dismiss(loadingToast);
@@ -37,12 +44,19 @@ const BlogEditor = () => {
             e.preventDefault();
         }
     }
-    const handleTitle = (e) => {
+    const handleTitleChange = (e) => {
         const input = e.target;
         input.style.height = 'auto';
         input.style.height = input.scrollHeight + "px";
+             console.log(input.value)
+        setBlog({...blog,title:input.value})
     }
 
+    const handleError = (e)=>{
+        let img = e.target;
+        console.log(img)
+        img.src= defaultBanner;
+    }
     return (
         <>
             <nav className="navbar" >
@@ -50,8 +64,9 @@ const BlogEditor = () => {
                     <img src={logo} className="flex-none w-12" />
                 </Link>
 
-                <p className="max-md:hidden text-black line-clamp-1 w-full">
-                    New Blog
+         {/* //removed max-md:hidden */}
+                <p className=" text-black line-clamp-1 w-full"> 
+                    {title.length ? title: "New Blog"}
                 </p>
 
                 <div className="flex gap-4 ml-auto">
@@ -72,8 +87,9 @@ const BlogEditor = () => {
                         <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
                             <label htmlFor="uploadBanner">
                                 < img
-                                    ref={blogBannerRef}
-                                    src={defaultBanner} />
+                                    src={banner}
+                                    onError={handleError}
+                                    />
                                 <input
                                     id="uploadBanner"
                                     type="file"
@@ -88,7 +104,7 @@ const BlogEditor = () => {
                             className="mt-10 text-4xl font-medium w-full h-20 outline-none resize-none leading-tight 
                         placeholder:opacity-40  "
                             onKeyDown={handleKeyDown}
-                            onChange={handleTitle}
+                            onChange={handleTitleChange}
                         ></textarea>
                     </div>
                 </section>
