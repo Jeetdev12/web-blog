0e20146aa5b0c8e93e02dee60285b7df4bd1e9bf
Your Name <your.email@example.com>
2025-09-24 23:26:38 +0530

    home page button setup

--- frontend/src/components/inpage-navigation.component.jsx
+++ frontend/src/components/inpage-navigation.component.jsx
@@ -1,17 +1,45 @@
+import { useEffect, useRef, useState } from "react";
 
 
 
-const InPageNavigation= ({routes})=>{
- console.log(routes)
-return(
-    <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
-    { routes.map((route,i)=>(
-        <button className="m-4">
-            {route}
-       </button>
-     ))}
-    </div>
-)
+const InPageNavigation = ({ routes,defaultActiveIndex = 0, defaultHidden=[], children }) => {
+
+    const [inPageNavIndex, setInPageNavIndex] = useState(defaultActiveIndex);
+    const activeTabLineRef = useRef();
+    const activeTabRef = useRef();
+ console.log(defaultHidden.includes("Trending Blogs") ? " md:hidden":'null')
+    // dark underline under active button
+    const handleBtnClick = (btn, i) => {
+
+        const { offsetWidth, offsetLeft } = btn;
+        activeTabLineRef.current.style.width = offsetWidth + "px";
+        activeTabLineRef.current.style.left = offsetLeft + "px";
+        setInPageNavIndex(i)
+    }
+
+    useEffect(()=>{
+       handleBtnClick(activeTabRef.current,defaultActiveIndex)
+    },[])
+
+    return (
+        <>
+            <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
+                {routes.map((route, i) => (
+                    <button key={i} className={"p-4 px-5 capitalize " + (inPageNavIndex == i ? "text-black " : "text-dark-grey ") + (defaultHidden.includes(route) ? " md:hidden":'')}
+                        onClick={(e) => handleBtnClick(e.target, i)}
+
+                        ref={defaultActiveIndex == i? activeTabRef:null}
+                    >
+                        {route}
+                    </button>
+                ))}
+
+                <hr ref={activeTabLineRef} className="absolute bottom-0 duration-300" />
+            </div>
+
+            {Array.isArray(children) ? children[inPageNavIndex] :children}
+        </>
+    )
 
 }
 
--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -7,12 +7,17 @@ export default function HomePage() {
 
     return (
         <AnimationWrapper>
-            <section className="hcover flex  justify-center gap-10 ">
-                <div >
-                    <InPageNavigation routes={["Home","Trending Blogs"]}>
+            <section className="h-cover flex  justify-center gap-10 ">
+                {/* Latest blogs */}
+                <div className="w-full">
+                    <InPageNavigation routes={["Home","Trending Blogs"]} defaultHidden={["Trending Blogs"]}>
+
+           <h1>Latest blogs here</h1>
+           <h1>Trending blogs here</h1>
 
                     </InPageNavigation>
                 </div>
+                
                 <div>
 
                 </div>
